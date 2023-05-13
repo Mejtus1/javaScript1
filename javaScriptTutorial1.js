d93537a5d1d2f6df29c33825607525e2cda339d4
Mejtus1 <GitMaster1769@proton.me>
2023-05-13 12:44:35 +0200

    adding info

--- javaScriptTutorial1.js
+++ javaScriptTutorial1.js
@@ -1,1 +1,62 @@
 var //global variable, can be used outside of the scope of assigment 
+let // local variable, value can be changed througth code
+const // local variable, value cannot be changed (constant)
+
+// Data Types: 
+// undefined, null, string, number, symbol, boolean and object 
+
+var a;
+var b = 2;
+console.log(a)
+a = 7;
+
+b = a;
+
+console.log(a)   // with this we can track how our variables are beaing used throughtout our program 
+
+// ----------------------------------------------------------------------------------- // 
+
+var a = 9 // initialized variable 
+var b // uninitialized variable 
+
+// ----------------------------------------------------------------------------------- // 
+// Declarations 
+var StUdyCApvAr; // javascript is case sensitive
+var studlyCapVar; // it is general practise to use Camel case - using Capital letter for every new word
+// Assigments 
+studlyCapVar = 10; 
+
+// ----------------------------------------------------------------------------------- // 
+// Adding numbers 
+var sum = 10 + 10;
+console.log(sum)
+// Subraction 
+var difference 45 - 33;
+console.log(difference)
+// Multiplication 
+var product = 8 * 10;
+console.log(product)
+// Divide
+var qoutient = 64 / 2;
+console.log(qoutient)
+// increment a number (adding 1 to it)
+var myVar = 87; 
+myVar = Myvar + 1;
+myVar++ // is increments the value. does the same like code above 
+// decrementing value (subtacting 1)
+var MyVar = 85;
+MyVar--
+//Decimal numbers
+var ourDecimal = 5.7; 
+var myDecimal = 0.000008;
+// Multiplying decimals 
+var product = 2.01 * 0.0007;
+//Divide decimals 
+var quotient1 = 2.5 / 7.5;
+// Remainder (remainder of the division of two numbers)
+var remainder; 
+remainder = 11 % 3;
+// Compound Assigement with Augmented Addition 
+VAR H = 3;
+H = H + 15; // it is common operation so there is a shortcut 
+H *= 12
