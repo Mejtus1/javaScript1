79a2e929bab0cf56a7c101fc3c3b6c55cbd02819
Mejtus1 <GitMaster1769@proton.me>
2023-05-13 20:07:16 +0200

    adding javaScriptTutorial3.js file with first notes on functions

---
+++ javaScriptTutorial3.js
@@ -0,0 +1,9 @@
+// Functions 
+// Writing reusable code with functions 
+
+function ourReusableFunction() {  // there always need to be the parenthesis ()
+    console.log("Hello, World");  // everything inside the curly bracket is run 
+}                                 // everything inside function goes insode {} 
+ourReusableFunction;              // runs hello world in the console 
+
+//53:40
