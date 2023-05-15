a066f5bcab65913e103fb4c249c1917b4210b382
Mejtus1 <GitMaster@proton.me>
2023-05-15 23:25:19 +0200

    Objects

---
+++ javaScriptTutorial5.js
@@ -0,0 +1,18 @@
+//Build javaScript Objects 
+//Objects are simmilar to arrays, BUT instead of using indexes to access data you use properties 
+//Object ourDog
+var ourDog = {                  //Objects are defined with curly braces, at the beginning and the end 
+    "name": "Camper";           
+    "legs": 4,                  //name, legs, tails, friends are object properties, properties are everything before the colons :
+    "tails": 1,
+    "friends": ["everything!"]  //properties can be any data type in javascript 
+};
+
+var myDog = {
+    "name": = "John"
+    "legs": = 4,
+    "tails": = 1,
+    "friends": []
+};
+
+// Accessing Object Properties with Dot notation
