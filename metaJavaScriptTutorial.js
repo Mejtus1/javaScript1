fd50fbb4311b5cc2ce9bb2dcab2676b68b8bd177
Mejtus1 <GitMaster1769@proton.me>
2023-08-28 09:26:14 +0200

    exercise

--- metaJavaScriptTutorial.js
+++ metaJavaScriptTutorial.js
@@ -816,6 +816,35 @@ input.addEventListener('change', function() {
     h1.innerText = input.value
 })
 
+//example 
+//we can use www.example.com for trying out different javascript techiques like this one 
+var h1 = document.querySelector('h1')
+
+var arr = [
+    'Example Domain',
+    'First Click',
+    'Second Click',
+    'Third Click'
+    ]
+
+    function handleClicks() {
+            switch(h1.innerText) {
+                case arr[0]:
+                    h1.innerText = arr[1]
+                    break
+                case arr[1]:
+                    h1.innerText = arr[2]
+                    break
+                case arr[2]:
+                    h1.innerText = arr[3]
+        break
+        default:
+        h1.innerText = arr[0]
+        }
+        }
+
+        h1.addEventListener('click', handleClicks);
+
 
 
 
