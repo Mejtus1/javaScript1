280b79f7bf83da17ed0539a955ea2ef5842ea219
Mejtus1 <GitMaster1769@proton.me>
2023-08-29 00:09:09 +0200

    JSON format

--- metaJavaScriptTutorial.js
+++ metaJavaScriptTutorial.js
@@ -845,6 +845,71 @@ var arr = [
 
         h1.addEventListener('click', handleClicks);
 
+//Moving data around on the web
+//JSON is a data format in which data can be captured and used in javascript objects 
+//most used format before that was XML 
+//Not all javascript code is JSON and not all JSON is javascript code, but we can use JSON format
+//with additional conversions: example:
+const currencyInfo = {
+    [
+        USD: {
+            // ...
+        },
+        GBP: {
+            // ...
+        },
+        EUR: {
+            // ...
+        }
+    ]
+}
+//here we have a currency info in an array downloaded from the server 
 
+//It's all about getting stringified JSON data from a server, converting ("parsing") that data 
+//into JS objects in your own code, working with the converted data in your own code, and 
+//perhaps even stringifying the result into JSON, so that this data is then ready to, 
+//for example, be sent back to a server after your code has processed it locally
+
+
+//JSON is just a string - but there are rules that it must follow
+
+/*JSON is a string, but it must be a properly-formatted string. In other words, it must adhere to some rules.
+If a JSON string is not properly formatted, JavaScript would not be able to parse it into a JavaScript object.
+JSON can work with some primitives and some complex data types, as described below.
+Only a subset of values in JavaScript can be properly stringified to JSON and parsed from a JavaScript object into a JSON string.
+
+These values include:
+primitive values: strings, numbers, bolleans, null
+complex values: objects and arrays (no functions!)
+
+Objects have double-quoted strings for all keys
+
+Properties are comma-delimited both in JSON objects and in JSON arrays, just like in regular JavaScript code
+String properties must be surrounded in double quotes. For example:
+"fruits",
+"vegetables"
+
+Number properties are represented using the regular JavaScript number syntax; e.g
+5,
+10,
+1.2
+
+Boolean properties are represented using the regular JavaScript boolean syntax, that is:
+true
+false
+
+Null as a property is the same as in regular JavaScript; it's just a
+null
+
+You can use object literals and array literals, as long as you follow the above rules
+
+What happens if you try to stringify a data type which is not accepted in JSON syntax?
+For example, what if you try to stringify a function? The operation will silently fail. */
+
+//Examples of JSON format 
+/* '{"color":"red"}' = stringified JSON object
+   '["one", "two", "three"]' stringified JSON array 
+   '[{ "color": "blue" }, {"color: "red"}]' = stringified JSON array holding objects with key value pairs*/
+ 
 
 
